78003d8ee6118a1bc831c99a6f7f67e5659a7aea
francesco.miceli <francescomiceli@outlook.com>
2022-09-12 15:24:12 +0200

    Esercizio completo!

--- my.js
+++ my.js
@@ -5,6 +5,7 @@ new Vue(
     el: '#message',
     data:{
         welcome: 'Hello World',
+        path: 'https://www.posizionamento-seo.com/wp-content/uploads/2015/11/motore-di-ricerca-google.jpg',
     }
 }
 ); 
